928f0a440969ecf10f70d2afd8f1eadf684d1829
SALLAH-JP <jeanpaulsallah@gmail.com>
2024-03-14 11:11:15 +0400

    v0.2

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 
@@ -16,15 +17,23 @@ int main() {
 	
 	int valeur_a_trouver = rand() % 100;
 	int choix_joueur;
+	int choix_mode;
 	
+	printf("Veuillez choisir un mode de difficulté.\nEntrez 0 pour le mode facile, 25 pour le mode moyen ou 10 pour le mode difficile : ");
+	scanf("%i", &choix_mode);
 	
+	int Compteur = 1;
 	do {
 		printf("Veuillez entrez la valeur que vous avez choisi : ");
 		scanf("%i", &choix_joueur);
 		
 		plus_moins(choix_joueur, valeur_a_trouver);
 		printf("\n");
-	} while (choix_joueur != valeur_a_trouver);
+		
+		Compteur++;
+	} while (choix_joueur != valeur_a_trouver && Compteur != choix_mode);
+	
+	if (choix_joueur != valeur_a_trouver) printf("Vous avez perdu");
 	
 	
 }
