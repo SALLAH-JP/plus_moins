7f1df515ba1fee8b481b968087d7fa903cbe1aba
SALLAH-JP <jeanpaulsallah@gmail.com>
2024-03-14 10:38:07 +0400

    v0.1

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -5,8 +5,8 @@
 
 void plus_moins(int choix_joueur, int valeur_a_trouver) {
 	
-	if (choix_joueur < valeur_a_trouver) printf("plus");
-	else if (choix_joueur > valeur_a_trouver) printf("moins");
+	if (choix_joueur < valeur_a_trouver) printf("plus\n");
+	else if (choix_joueur > valeur_a_trouver) printf("moins\n");
 	else printf("Bingo");
 	
 }
@@ -16,7 +16,15 @@ int main() {
 	
 	int valeur_a_trouver = rand() % 100;
 	int choix_joueur;
-	scanf("%i", &choix_joueur);
 	
-	plus_moins(choix_joueur, valeur_a_trouver);
+	
+	do {
+		printf("Veuillez entrez la valeur que vous avez choisi : ");
+		scanf("%i", &choix_joueur);
+		
+		plus_moins(choix_joueur, valeur_a_trouver);
+		printf("\n");
+	} while (choix_joueur != valeur_a_trouver);
+	
+	
 }
