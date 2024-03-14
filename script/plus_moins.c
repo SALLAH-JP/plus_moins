ed1ff9f01895e1c883e86ced06ffba255a1804ca
SALLAH-JP <jeanpaulsallah@gmail.com>
2024-03-14 10:23:34 +0400

    La valeur a trouver devient aléatoire

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 void plus_moins(int choix_joueur, int valeur_a_trouver) {
 	
@@ -9,7 +12,9 @@ void plus_moins(int choix_joueur, int valeur_a_trouver) {
 }
 
 int main() {
-	int valeur_a_trouver = 769;
+	srand (time (0));
+	
+	int valeur_a_trouver = rand() % 100;
 	int choix_joueur;
 	scanf("%i", &choix_joueur);
 	
